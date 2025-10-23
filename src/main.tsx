f5ceb2ddc3a92b03cceba286c9e72b0622f815b3
Jasper Raine <jasper.raine@gmail.com>
2025-10-23 21:43:07 +0100

    add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { StoreProvider } from './store/StoreProvider'
+import * as Sentry from "@sentry/react";
+
+
+Sentry.init({
+  dsn: "https://5f9de741e5e64dd1791d62625186f335@o4510216814133248.ingest.de.sentry.io/4510216815444048",
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
